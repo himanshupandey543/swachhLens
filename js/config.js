c344620ae5901c45fc532f538e37a0e506776d41
Himanshu <hp184020@gmail.com>
2026-08-16 14:39:09 +0530

    Fix API URL to include /api path

--- js/config.js
+++ js/config.js
@@ -8,6 +8,6 @@ window.SW_CONFIG = {
   APP_NAME: 'SwachLens',
   TICKER: 'Smart Waste Management',
   API_URL: window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
-    ? 'http://localhost:8000'
-    : window.location.origin,
+    ? 'http://localhost:8000/api'
+    : window.location.origin + '/api',
 };
